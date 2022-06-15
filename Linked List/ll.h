6bbd6933aa4126b337acc057382f1a13e285dc1a
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-06-14 17:38:46 -0700

    2022/06/14 17:30

--- Linked List/ll.h
+++ Linked List/ll.h
@@ -7,3 +7,21 @@ struct LLNode {
 typedef struct LLNode LLNode;
 
 LLNode* createNew(int val);
+
+int getLength(LLNode* node);
+
+int findFirst(int element, LLNode* node);
+
+int findLast(int element, LLNode* node);
+
+void push(LLNode* node, int val);
+
+void pop(LLNode* node);
+
+void clean(LLNode* node);
+
+void enqueue(LLNode* node, int val);
+
+void dequeue(LLNode* node);
+
+void to_string(LLNode* node);
