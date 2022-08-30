da965fb614f700c17fa1492cb5dd5a68ee9ef892
MonsterBlue01 <73154603+MonsterBlue01@users.noreply.github.com>
2022-08-30 16:16:18 -0700

    2022/08/30 16:30

--- Hashtable/ht.c
+++ Hashtable/ht.c
@@ -15,20 +15,32 @@ entry* initialize() {
         tmp->down = (entry *)malloc(sizeof(entry));
         tmp->down->up = tmp;
         tmp->down->index = index++;
-        printf("Index: %d\n", index);
     }
     return e;
 }
 
+void clean(entry* e) {
+    while (e->up != NULL) {
+        e = e->up;
+    }
+    while (e != NULL) {
+        entry* tmp = e;
+        free(tmp);
+        e = e->down;
+    }
+}
+
 int main() {
     entry* e = initialize();
-    entry* tmp = e;
-    while (tmp->down != NULL) {
-        printf("Index down: %d\n", tmp->index);
-        tmp = tmp->down;
-    }
-    while (tmp->up != NULL) {
-        printf("Index up: %d\n", tmp->index);
-        tmp = tmp->up;
-    }
+    // entry* tmp = e;
+    // while (tmp->down != NULL) {
+    //     printf("Index down: %d\n", tmp->index);
+    //     tmp = tmp->down;
+    // }
+    // while (tmp->up != NULL) {
+    //     printf("Index up: %d\n", tmp->index);
+    //     tmp = tmp->up;
+    // }
+
+    clean(e);
 }
